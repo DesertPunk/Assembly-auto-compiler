1153cdd468e7ff0d5f8b47394aed79dfd1a3c398
DesertPunk <38014530+DesertPunk@users.noreply.github.com>
2019-03-25 00:58:02 +0800

    Delete assembly-x64-compiler.sh

--- assembly-x64-compiler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#assembly auto compiler script
-echo what do you want the input file to be?
-read input
-echo what do you want the output file to be called?
-read output
-nasm -f elf64 -o jizz-file.o $input && ld jizz-file.o -o $output
